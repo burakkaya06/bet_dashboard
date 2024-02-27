bf389222d0662417287b8ac16e67ea1885db33de
burakkaya06 <hural061995@gmail.com>
2024-02-27 10:48:34 +0300

    new improve

--- app/Service/DashboardService.php
+++ app/Service/DashboardService.php
@@ -19,15 +19,15 @@ class DashboardService
 
     public function getProfit ()
     {
-        $profit = DB::select('select (select first_balance from win_simulator where  active !=0 order by id desc limit 1) -
-       (select first_balance from win_simulator where  active !=0 order by id asc limit 1) as kar');
+        $profit = DB::select('select (select first_balance from win_simulator where  active is  null order by id desc limit 1) -
+       (select first_balance from win_simulator where  active is  null order by id asc limit 1) as kar');
         return $profit[ 0 ]->kar;
     }
 
     public function getMaxN ()
     {
         try {
-            $maxN = DB::select('select n from win_simulator where n = (select max(n) from win_simulator where  active !=0) order by  id  desc   limit 1');
+            $maxN = DB::select('select n from win_simulator where n = (select max(n) from win_simulator where  active is  null) order by  id  desc   limit 1');
             return $maxN[ 0 ]->n;
 
         } catch ( \Exception $e ) {
@@ -39,7 +39,7 @@ class DashboardService
     public function getLastN ()
     {
         try {
-            $lastN = DB::select('select n from win_simulator where  active !=0 order by  id  desc   limit 1');
+            $lastN = DB::select('select n from win_simulator where  active is  null order by  id  desc   limit 1');
             return $lastN[ 0 ]->n;
 
         } catch ( \Exception $e ) {
@@ -50,7 +50,7 @@ class DashboardService
     public function getLastNCreatedAt ()
     {
         try {
-            $lastNCreatedAt = DB::select('select created_at from win_simulator where n = (select max(n) from win_simulator where  active !=0) order by  id  desc   limit 1');
+            $lastNCreatedAt = DB::select('select created_at from win_simulator where n = (select max(n) from win_simulator where  active is  null) order by  id  desc   limit 1');
             return $lastNCreatedAt[ 0 ]->created_at;
 
         } catch ( \Exception $e ) {
@@ -61,7 +61,7 @@ class DashboardService
     public function getLastColor ()
     {
         try {
-            $lastColor = DB::select('select * from win_simulator  where  active !=0 order by  id  desc   limit 1');
+            $lastColor = DB::select('select * from win_simulator  where  active is  null order by  id  desc   limit 1');
             if ( $lastColor[ 0 ]->process == '-' ) {
                 if ( $lastColor[ 0 ]->colour == 'siyah' )
                     return 'Kırmızı';
@@ -76,7 +76,7 @@ class DashboardService
     public function getLastNumber ()
     {
         try {
-            $lastNumber = DB::select('select sayi from win_simulator where active !=0  order by  id  desc   limit 1');
+            $lastNumber = DB::select('select sayi from win_simulator where active is  null  order by  id  desc   limit 1');
             return $lastNumber[ 0 ]->sayi;
 
         } catch ( \Exception $e ) {
@@ -87,7 +87,7 @@ class DashboardService
     public function getPlayedColor ()
     {
         try {
-            $lastColour = DB::select('select colour from win_simulator where  active !=0  order by  id  desc   limit 1');
+            $lastColour = DB::select('select colour from win_simulator where  active is  null  order by  id  desc   limit 1');
             return $lastColour[ 0 ]->colour;
 
         } catch ( \Exception $e ) {
@@ -98,7 +98,7 @@ class DashboardService
     public function getState ()
     {
         try {
-            $state = DB::select('select * from win_simulator where active !=0  order by  id  desc   limit 1');
+            $state = DB::select('select * from win_simulator where active is  null  order by  id  desc   limit 1');
             if ( $state[ 0 ]->process == '+' ) {
                 return 'Kazandı';
             }
@@ -112,7 +112,7 @@ class DashboardService
     public function getDate ()
     {
         try {
-            $date = DB::select('select created_at from win_simulator where active !=0  order by  id  desc   limit 1');
+            $date = DB::select('select created_at from win_simulator where active is  null  order by  id  desc   limit 1');
             return $date[ 0 ]->created_at;
 
         } catch ( \Exception $e ) {
@@ -125,9 +125,9 @@ class DashboardService
         try {
             $date = DB::select('
         SELECT
-  TIMESTAMPDIFF(HOUR, (SELECT created_at FROM win_simulator WHERE id > 1 and active !=0 ORDER BY id ASC LIMIT 1), NOW()) DIV 24 AS gun,
-  TIMESTAMPDIFF(HOUR, (SELECT created_at FROM win_simulator WHERE id > 1 active !=0 ORDER BY id ASC LIMIT 1), NOW()) % 24 AS saat,
-  TIMESTAMPDIFF(MINUTE, (SELECT created_at FROM win_simulator WHERE id > 1 active !=0 ORDER BY id ASC LIMIT 1), NOW()) % 60 AS dakika;
+  TIMESTAMPDIFF(HOUR, (SELECT created_at FROM win_simulator WHERE id > 1 and active is  null ORDER BY id ASC LIMIT 1), NOW()) DIV 24 AS gun,
+  TIMESTAMPDIFF(HOUR, (SELECT created_at FROM win_simulator WHERE id > 1  and active is  null ORDER BY id ASC LIMIT 1), NOW()) % 24 AS saat,
+  TIMESTAMPDIFF(MINUTE, (SELECT created_at FROM win_simulator WHERE id > 1 and active is  null ORDER BY id ASC LIMIT 1), NOW()) % 60 AS dakika;
 ');
             return $date[ 0 ]->gun . ' Gün ' . $date[ 0 ]->saat . ' Saat ' . $date[ 0 ]->dakika . ' Dakika';
 
@@ -139,7 +139,7 @@ class DashboardService
     public function getStartDate ()
     {
         try {
-            $date = DB::select('select created_at from win_simulator where active !=0  order by  id  asc   limit 1');
+            $date = DB::select('select created_at from win_simulator where active is  null  order by  id  asc   limit 1');
             return $date[ 0 ]->created_at;
         } catch ( \Exception $e ) {
             return null;
